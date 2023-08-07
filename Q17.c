a8558946607a99a1000164a80df69d1451fbcd5e
ameencaslam <120704619+ameencaslam@users.noreply.github.com>
2023-08-07 14:52:49 +0530

    Update Q17.c

--- Q17.c
+++ Q17.c
@@ -4,25 +4,28 @@
 
 void main()
 {
-    int i,ar1[5],ar2[5],ar3[10];
-    
-    printf("Enter 5 elements for Array 1: ");
-    
-    for(i=0;i<5;i++)
+    int i,n1,n2;
+    printf("Enter number of entries for Array 1: ");
+    scanf("%d",&n1);
+    printf("Enter number of entries for Array 2: ");
+    scanf("%d",&n2);
+    int ar1[n1],ar2[n2],ar3[n1+n2];
+    printf("Enter elements of Array 1: ");
+    for(i=0;i<n1;i++)
     {
         scanf("%d",&ar1[i]);
         ar3[i]=ar1[i];
     }
 
-    printf("Enter 5 elements for Array 2: ");
-    for(i=0;i<5;i++)
+    printf("Enter elements for Array 2: ");
+    for(i=0;i<n2;i++)
     {
         scanf("%d",&ar2[i]);
-        ar3[i+5]=ar2[i];
+        ar3[i+n1]=ar2[i];
     }
 
     printf("Appended Array: ");
-    for(i=0;i<10;i++)
+    for(i=0;i<(n1+n2);i++)
     {
         printf("%d ",ar3[i]);
     }
